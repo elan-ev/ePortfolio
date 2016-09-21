b12cd7ac45c1dea59dfd3c7849af44854bd915a3
marcelkipp <mkipp@uos.de>
2016-09-21 07:27:46 +0200

    upgrade to newest version in studip

---
+++ migrations/002_add_sem_class.php
@@ -0,0 +1,109 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+class AddSemClass extends Migration
+{
+    public function description () {
+        return 'add SemClass and SemTypes whose courses have this plugin in their overview slot';
+    }
+
+
+    public function up () {
+        $id = $this->insertSemClass();
+        SimpleORMap::expireTableScheme();
+    }
+
+
+    public function down () {
+        // $id = $this->getMoocSemClassID();
+        // $this->removeSemClassAndTypes($id);
+        // $this->removeConfigOption();
+        // SimpleORMap::expireTableScheme();
+
+        $db = DBManager::get();
+
+        //remove entry in sem_classes
+        $name = "ePortfolio";
+        $statement = $db->prepare("DELETE FROM sem_classes WHERE name = ?");
+        $statement->execute(array($name));
+
+        //remove entry in sem_types
+        $nameType = "ePortfolio";
+        $statement = $db->prepare("DELETE FROM sem_types WHERE name = ?");
+        $statement->execute(array($nameType));
+    }
+
+    /**********************************************************************/
+    /* PRIVATE METHODS                                                    */
+    /**********************************************************************/
+
+
+    private function insertSemClass()
+    {
+        $db = DBManager::get();
+        $name = "ePortfolio";
+        $nameType = "ePortfolio";
+        $id = -2;
+
+        if($this->validateUniqueness($name)) {
+    			$statement = $db->prepare("INSERT INTO sem_classes SET name = ?, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
+    			$statement->execute(array($name));
+    			$id = $db->lastInsertId();
+
+          //Insert sem_type
+          $statementSemTypes = $db->prepare("INSERT INTO sem_types SET name = ?, class = $id, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
+          $statementSemTypes->execute(array($nameType));
+
+    	    } else {
+    			// We already got a type with that name, should be a previous installation ...
+                $statement = $db->prepare('SELECT id FROM sem_classes WHERE name = ?');
+                $statement->execute(array($name));
+                $id = $statement->fetchColumn();
+    		}
+
+    		if($id === -2) {
+    			$message = sprintf('Ung�ltige id (id=%d)', $id);
+                throw new Exception($message);
+    		}
+
+
+        $sem_class = SemClass::getDefaultSemClass();
+        $sem_class->set('name', $name);
+        $sem_class->set('id', $id);
+
+        // Setting Mooc-courses default datafields: mooc should not to be disabled, courseware and mooc should be active
+        $current_modules = $sem_class->getModules(); // get modules
+        $current_modules['Mooc']['activated'] = '0'; // set values
+        $current_modules['Mooc']['sticky'] = '1'; // sticky = 1 -> can't be chosen in "more"-field of course
+        $current_modules['Courseware']['activated'] = '1';
+        $current_modules['Courseware']['sticky'] = '1';
+
+
+        $sem_class->setModules($current_modules); // set modules
+
+        $sem_class->store();
+
+        return $id;
+    }
+
+    private function validateUniqueness($name)
+    {
+        $statement = DBManager::get()->prepare('SELECT id FROM sem_classes WHERE name = ?');
+        $statement->execute(array($name));
+        if ($old = $statement->fetchColumn()) {
+            // $message = sprintf('Es existiert bereits eine Veranstaltungskategorie mit dem Namen "%s" (id=%d)', htmlspecialchars($name), $old);
+            // throw new Exception($message);
+            return false;
+        }
+        return true;
+    }
+
+    private function removeSemClassAndTypes($id)
+    {
+        $sem_class = new SemClass(intval($id));
+        $sem_class->delete();
+        $GLOBALS['SEM_CLASS'] = SemClass::refreshClasses();
+    }
+
+}
