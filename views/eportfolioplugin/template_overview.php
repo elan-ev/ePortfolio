504073856c1e275d0e5cd373135eb0add219d485
marcelkipp <mkipp@uos.de>
2016-10-31 14:01:29 +0100

    reset form after send

--- views/eportfolioplugin/template_overview.php
+++ views/eportfolioplugin/template_overview.php
@@ -1,1 +1,61 @@
-<h2>include template</h2>
+<h1>Portfolio Name</h1>
+
+<div class="row">
+  <div class="col-md-4" style=";padding: 5px; padding-bottom: 50px;">
+    <div class="" style="padding: 5px; padding-bottom: 50px; border:1px solid grey;">
+
+    <h4>Reflexionsimpuls 1</h4>
+
+    <div class="" style="background-color:rgba(0,0,0,0.2);width:100%;height: 150px;">
+      Bild
+    </div>
+
+    <div class="alert alert-info" style="margin: 20px 0;" role="alert">Warum will ich Lehrerin werden? Welche Staerken will ich einbringen? </div>
+
+    <div class="">
+      <b>Freigaben: </b>50 <br>
+      <b>Kommentare: </b> 12
+    </div>
+
+  </div>
+</div>
+
+<div class="col-md-4" style=";padding: 5px; padding-bottom: 50px;">
+  <div class="" style="padding: 5px; padding-bottom: 50px; border:1px solid grey;">
+
+  <h4>Reflexionsimpuls 1</h4>
+
+  <div class="" style="background-color:rgba(0,0,0,0.2);width:100%;height: 150px;">
+    Bild
+  </div>
+
+  <div class="alert alert-info" style="margin: 20px 0;" role="alert">Warum will ich Lehrerin werden? Welche Staerken will ich einbringen? </div>
+
+  <div class="">
+    <b>Freigaben: </b>50 <br>
+    <b>Kommentare: </b> 12
+  </div>
+
+</div>
+</div>
+
+<div class="col-md-4" style=";padding: 5px; padding-bottom: 50px;">
+  <div class="" style="padding: 5px; padding-bottom: 50px; border:1px solid grey;">
+
+  <h4>Reflexionsimpuls 1</h4>
+
+  <div class="" style="background-color:rgba(0,0,0,0.2);width:100%;height: 150px;">
+    Bild
+  </div>
+
+  <div class="alert alert-info" style="margin: 20px 0;" role="alert">Warum will ich Lehrerin werden? Welche Staerken will ich einbringen? </div>
+
+  <div class="">
+    <b>Freigaben: </b>50 <br>
+    <b>Kommentare: </b> 12
+  </div>
+
+</div>
+</div>
+
+</div>
