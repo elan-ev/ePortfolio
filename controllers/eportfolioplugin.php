5bb1f3ffa9b85d40e8fb1beeb98e4bbcfd267102
marcelkipp <mkipp@uos.de>
2016-11-01 14:52:08 +0100

    overview in seminar/ output template

--- controllers/eportfolioplugin.php
+++ controllers/eportfolioplugin.php
@@ -11,28 +11,32 @@ class EportfoliopluginController extends StudipController {
       Sidebar::Get()->setTitle('Uebersicht');
       $widget = new SearchWidget();
       Sidebar::Get()->addWidget($widget);
-
   }
 
   public function before_filter(&$action, &$args)
   {
       parent::before_filter($action, $args);
 
-      $this->set_layout($GLOBALS['template_factory']->open('layouts/base.php'));
-      PageLayout::setTitle('Create');
+      // $this->set_layout($GLOBALS['template_factory']->open('layouts/base.php'));
+      PageLayout::setTitle('Uebersicht');
 
   }
 
 
   public function index_action()
   {
-    //set AutoNavigation
-    Navigation::activateItem("course/eportfolioplugin");
 
+    //set AutoNavigation/////
+    Navigation::activateItem("course/eportfolioplugin");
+    ////////////////////////
+
+    $userid = $GLOBALS["user"]->id;
     $cid = $_GET["cid"];
     $i = 0;
+    $isOwner = false;
 
     $db = DBManager::get();
+    $this->plugin = $dispatcher->plugin;
 
     // get template Status
     $templateStatus = $db->query("SELECT templateStatus FROM eportfolio WHERE Seminar_id = '$cid' ")->fetchAll();
@@ -41,6 +45,17 @@ class EportfoliopluginController extends StudipController {
     // get courseware parentId
     $getCourseware = $db->query("SELECT id FROM mooc_blocks WHERE type = 'Courseware' AND seminar_id = '$cid'")->fetchAll();
     $getC = $getCourseware[0][id];
+
+    //get seninar infos
+    $getSeminarInfo = $db->query("SELECT name FROM seminare WHERE Seminar_id = '$cid'")->fetchAll();
+    $getS = $getSeminarInfo[0][name];
+
+    //check if owner
+    $queryIsOwner = $db->query("SELECT owner_id FROM eportfolio WHERE Seminar_id = '$cid'")->fetchAll();
+    $ownerId = $queryIsOwner[0][owner_id];
+    if($userid == $ownerId){
+      $isOwner = true;
+    }
 
     //auto insert chapters
     if ($t == 0) {
@@ -52,27 +67,42 @@ class EportfoliopluginController extends StudipController {
         $db->query("UPDATE eportfolio SET templateStatus = '1' WHERE seminar_id = '$cid'");
         $i++;
       }
-
     }
+
+    //get cardinfos for overview
+    $return_arr = array();
+    $getCardInfos = $db->query("SELECT id, title FROM mooc_blocks WHERE seminar_id = '$cid' AND type = 'Chapter'")->fetchAll();
+    foreach ($getCardInfos as $value) {
+      $arrayOne = array();
+      $arrayOne['id'] = $value[id];
+      $arrayOne['title'] = $value[title];
+
+      array_push($return_arr, $arrayOne);
+    }
+
+    //push to template
+    $this->cardInfo = $return_arr;
+    $this->seminarTitle = $getS;
+    $this->isOwner = $isOwner;
 
   }
 
   // customized #url_for for plugins
-  function url_for($to)
-  {
-      $args = func_get_args();
-
-      # find params
-      $params = array();
-      if (is_array(end($args))) {
-          $params = array_pop($args);
-      }
-
-      # urlencode all but the first argument
-      $args = array_map('urlencode', $args);
-      $args[0] = $to;
-
-      return PluginEngine::getURL($this->dispatcher->plugin, $params, join('/', $args));
-
-  }
+  // function url_for($to)
+  // {
+  //     $args = func_get_args();
+  //
+  //     # find params
+  //     $params = array();
+  //     if (is_array(end($args))) {
+  //         $params = array_pop($args);
+  //     }
+  //
+  //     # urlencode all but the first argument
+  //     $args = array_map('urlencode', $args);
+  //     $args[0] = $to;
+  //
+  //     return PluginEngine::getURL($this->dispatcher->plugin, $params, join('/', $args));
+  //
+  // }
 }
--- views/eportfolioplugin/index.php
+++ views/eportfolioplugin/index.php
@@ -8,4 +8,42 @@
 
 </head>
 
-<?php include("template_overview.php"); ?>
+<h1 style="border:none!important;"><?php  echo $seminarTitle; ?></h1>
+<hr>
+
+<div class="row">
+
+<?php foreach ($cardInfo as $key): ?>
+
+  <?php
+    $link = '/studip/plugins.php/courseware/courseware?cid=r4noa4wa9l7lzhhy287uzb8lq7zvobmf&selected='.$key[id];
+    $linkAdmin = $link.'#author';
+  ?>
+
+  <div class="col-md-4 card-wrapper">
+    <div class="card-inner" style="">
+
+    <h4><?php echo $key[title]; ?></h4>
+
+    <div class="" style="background-color:rgba(0,0,0,0.2);width:100%;height: 150px;">
+      Bild
+    </div>
+
+    <div class="alert alert-info" style="margin: 20px 0;" role="alert">Warum will ich Lehrerin werden? Welche Staerken will ich einbringen? </div>
+
+    <div class="">
+      <b>Freigaben: </b>50 <br>
+      <b>Kommentare: </b> 12
+    </div>
+
+    <a href="<?php echo $link; ?>"><button type="button" class="btn btn-primary">Anschauen</button></a>
+
+    <?php if($isOwner == true):?>
+      <a href="<?php echo $linkAdmin; ?>"><button type="button" class="btn btn-primary">Bearbeiten</button></a>
+    <?php endif; ?>
+  </div>
+</div>
+
+<?php endforeach; ?>
+
+</div>
