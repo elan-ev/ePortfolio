0f3f4037c7c0592a16210cd21d02e31ca5b7d248
asudau <sudau@elan-ev.de>
2017-09-14 13:05:02 +0200

    additional semclasses and types: here they are (missing in last commit)

--- migrations/003_add_more_sem_classes.php
+++ migrations/003_add_more_sem_classes.php
@@ -2,15 +2,16 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-class AddSemClass extends Migration
+class AddMoreSemClasses extends Migration
 {
     public function description () {
-        return 'add SemClass and SemTypes whose courses have this plugin in their overview slot';
+        return 'add SemClass and SemTypes for Supervisionsgruppen and ePortfolio-Vorlagen';
     }
 
 
     public function up () {
-        $id = $this->insertSemClass();
+        $id = $this->insertSupervisionsgruppeSemClass();
+        $this->insertPortfolioVorlageSemClass();
         SimpleORMap::expireTableScheme();
     }
 
@@ -24,12 +25,21 @@ class AddSemClass extends Migration
         $db = DBManager::get();
 
         //remove entry in sem_classes
-        $name = "ePortfolio";
+        $name = "Supervisionsgruppe";
         $statement = $db->prepare("DELETE FROM sem_classes WHERE name = ?");
         $statement->execute(array($name));
 
         //remove entry in sem_types
-        $nameType = "ePortfolio";
+        $nameType = "Supervisionsgruppe";
+        $statement = $db->prepare("DELETE FROM sem_types WHERE name = ?");
+        $statement->execute(array($nameType));
+        //remove entry in sem_classes
+        $name = "ePortfolio-Vorlage";
+        $statement = $db->prepare("DELETE FROM sem_classes WHERE name = ?");
+        $statement->execute(array($name));
+
+        //remove entry in sem_types
+        $nameType = "ePortfolio-Vorlage";
         $statement = $db->prepare("DELETE FROM sem_types WHERE name = ?");
         $statement->execute(array($nameType));
     }
@@ -39,11 +49,11 @@ class AddSemClass extends Migration
     /**********************************************************************/
 
 
-    private function insertSemClass()
+    private function insertSupervisionsgruppeSemClass()
     {
         $db = DBManager::get();
-        $name = "ePortfolio";
-        $nameType = "ePortfolio";
+        $name = "Supervisionsgruppe";
+        $nameType = "Supervisionsgruppe";
         $id = -2;
 
         //FÃ¼gt Spalte an true or false ePortfolio
@@ -68,7 +78,7 @@ class AddSemClass extends Migration
     		}
 
     		if($id === -2) {
-    			$message = sprintf('Ungï¿½ltige id (id=%d)', $id);
+    			$message = sprintf('Ungültige id (id=%d)', $id);
                 throw new Exception($message);
     		}
 
@@ -91,6 +101,60 @@ class AddSemClass extends Migration
 
         return $id;
     }
+    
+    private function insertPortfolioVorlageSemClass()
+    {
+        $db = DBManager::get();
+        $name = "ePortfolio-Vorlage";
+        $nameType = "ePortfolio-Vorlage";
+        $id = -2;
+
+        //FÃ¼gt Spalte an true or false ePortfolio
+        // $nameType = "eportfolioStatus";
+        // $statement = $db->prepare("ALTER TABLE seminare ADD ? BOOLEAN");
+        // $statement->execute(array($nameType));
+
+        if($this->validateUniqueness($name)) {
+    			$statement = $db->prepare("INSERT INTO sem_classes SET name = ?, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
+    			$statement->execute(array($name));
+    			$id = $db->lastInsertId();
+
+          //Insert sem_type
+          $statementSemTypes = $db->prepare("INSERT INTO sem_types SET name = ?, class = $id, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
+          $statementSemTypes->execute(array($nameType));
+
+    	    } else {
+    			// We already got a type with that name, should be a previous installation ...
+                $statement = $db->prepare('SELECT id FROM sem_classes WHERE name = ?');
+                $statement->execute(array($name));
+                $id = $statement->fetchColumn();
+    		}
+
+    		if($id === -2) {
+    			$message = sprintf('Ungültige id (id=%d)', $id);
+                throw new Exception($message);
+    		}
+
+
+        $sem_class = SemClass::getDefaultSemClass();
+        $sem_class->set('name', $name);
+        $sem_class->set('id', $id);
+
+        // Setting Mooc-courses default datafields: mooc should not to be disabled, courseware and mooc should be active
+        $current_modules = $sem_class->getModules(); // get modules
+        $current_modules['Mooc']['activated'] = '0'; // set values
+        $current_modules['Mooc']['sticky'] = '1'; // sticky = 1 -> can't be chosen in "more"-field of course
+        $current_modules['Courseware']['activated'] = '1';
+        $current_modules['Courseware']['sticky'] = '1';
+
+
+        $sem_class->setModules($current_modules); // set modules
+
+        $sem_class->store();
+
+        return $id;
+    }
+    
 
     private function validateUniqueness($name)
     {
