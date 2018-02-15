ca65e19e7bc8614ed4bfa578b65cdc571ea3d091
Marcel Kipp <mkipp@uos.de>
2018-02-15 21:57:53 +0100

    #114 CSS gesammelt
    Bootsrap nur einmal eingebunden und CSS gesammelt

--- views/dozentview/index.php
+++ views/dozentview/index.php
@@ -11,17 +11,6 @@
 
 ?>
 
-
-<head>
-  <meta charset="utf-8"/><meta charset="utf-8"/>
-
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-</head>
-
 <div class="row">
 
   <div class="col-md-12">
--- views/show/index.php
+++ views/show/index.php
@@ -1,36 +1,3 @@
-
-
-
-<head>
-  <meta charset="utf-8"/><meta charset="utf-8"/>
-
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-  <style media="screen">
-
-    .supervisor-btn {
-      position: absolute;
-      right: 50px;
-      top: -2px;
-      padding: 8px;
-      color: #fff;
-      background-color: #28497c;
-      font-size: 20px;
-      border-top: 2px solid rgba(255,255,255,0.3);
-    }
-
-    .supervisor-btn a {
-      color: #fff;
-    }
-
-  </style>
-
-</head>
-
-
 <!-- Supervisor Button -->
 
 <?php if($linkId == 'noId'): ?>
