98ce2d21362f112caa06c1d4f5718f558dacc0b6
asudau <sudau@elan-ev.de>
2018-02-20 14:28:21 +0100

    remove semClass for supervisor-groups

--- migrations/003_add_more_sem_classes.php
+++ migrations/003_add_more_sem_classes.php
@@ -10,7 +10,6 @@ class AddMoreSemClasses extends Migration
 
 
     public function up () {
-        $id = $this->insertSupervisionsgruppeSemClass();
         $this->insertPortfolioVorlageSemClass();
     }
 
@@ -23,15 +22,7 @@ class AddMoreSemClasses extends Migration
 
         $db = DBManager::get();
 
-        //remove entry in sem_classes
-        $name = "Supervisionsgruppe";
-        $statement = $db->prepare("DELETE FROM sem_classes WHERE name = ?");
-        $statement->execute(array($name));
-
-        //remove entry in sem_types
-        $nameType = "Supervisionsgruppe";
-        $statement = $db->prepare("DELETE FROM sem_types WHERE name = ?");
-        $statement->execute(array($nameType));
+       
         //remove entry in sem_classes
         $name = "ePortfolio-Vorlage";
         $statement = $db->prepare("DELETE FROM sem_classes WHERE name = ?");
@@ -42,74 +33,7 @@ class AddMoreSemClasses extends Migration
         $statement = $db->prepare("DELETE FROM sem_types WHERE name = ?");
         $statement->execute(array($nameType));
     }
-
-    /**********************************************************************/
-    /* PRIVATE METHODS                                                    */
-    /**********************************************************************/
-
-
-    private function insertSupervisionsgruppeSemClass()
-    {
-        $db = DBManager::get();
-        $name = "Supervisionsgruppe";
-        $nameType = "Supervisionsgruppe";
-        $id = -2;
-
-        //Fügt Spalte an true or false ePortfolio
-        // $nameType = "eportfolioStatus";
-        // $statement = $db->prepare("ALTER TABLE seminare ADD ? BOOLEAN");
-        // $statement->execute(array($nameType));
-
-        if($this->validateUniqueness($name)) {
-    			$statement = $db->prepare("INSERT INTO sem_classes SET name = ?, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
-    			$statement->execute(array($name));
-    			$id = $db->lastInsertId();
-
-          //Insert sem_type
-          $statementSemTypes = $db->prepare("INSERT INTO sem_types SET name = ?, class = $id, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
-          $statementSemTypes->execute(array($nameType));
-
-    	    } else {
-    			// We already got a type with that name, should be a previous installation ...
-                $statement = $db->prepare('SELECT id FROM sem_classes WHERE name = ?');
-                $statement->execute(array($name));
-                $id = $statement->fetchColumn();
-    		}
-
-    		if($id === -2) {
-    			$message = sprintf('Ungültige id (id=%d)', $id);
-                throw new Exception($message);
-    		}
-
-
-        $sem_class = SemClass::getDefaultSemClass();
-        $sem_class->set('name', $name);
-        $sem_class->set('id', $id);
-
-        // Setting Mooc-courses default datafields: mooc should not to be disabled, courseware and mooc should be active
-        $current_modules = $sem_class->getModules(); // get modules
-        $current_modules = $sem_class->getModules(); // get modules
-        $current_modules['EportfolioPlugin']['activated'] = '1';
-        $current_modules['EportfolioPlugin']['sticky'] = '1'; 
-        $current_modules['Courseware']['activated'] = '1';   // set values
-        $current_modules['Courseware']['sticky'] = '1'; // sticky = 1 -> can't be chosen in "more"-field of course
-        $current_modules['CoreParticipants']['activated'] = '0';
-        $current_modules['CoreParticipants']['sticky'] = '0'; 
-        $current_modules['CoreDocuments']['activated'] = '1';
-        $current_modules['CoreDocuments']['sticky'] = '1'; 
-        $current_modules['CoreOverview']['activated'] = '0';
-        $current_modules['CoreOverview']['sticky'] = '1'; 
-        $current_modules['CoreAdmin']['activated'] = '0';
-        $current_modules['CoreAdmin']['sticky'] = '1';
-
-        $sem_class->set('overview', 'EportfolioPlugin');
-        $sem_class->setModules($current_modules); // set modules
-
-        $sem_class->store();
-
-        return $id;
-    }
-    
+  
     private function insertPortfolioVorlageSemClass()
     {
         $db = DBManager::get();
