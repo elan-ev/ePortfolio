10848eadf99148179edad9eb46e6dd15c3a41eb0
asudau <sudau@elan-ev.de>
2018-02-26 12:03:10 +0100

    portfolio-vorlagen export und import bleibt erstmal ajax #121

--- views/showsupervisor/createportfolio.php
+++ views/showsupervisor/createportfolio.php
@@ -1,16 +1,49 @@
-<form action="<?= URLHelper::getLink("plugins.php/courseware/exportportfolio?cid=". $masterid) ?>"
 
-      method="post" enctype="multipart/form-data"
-      <?= Request::isAjax() ? "data-dialog" : "" ?>>
-    
+<div data-dialog-button>
+    <?= \Studip\Button::create(_("Vorlage verteilen"), 'newvorlage', array('onclick' => "exportPortfolio('$masterid')", "data-dialog"=>"close")) ?>
+</div>
 
-     <div data-dialog-button>
-        <?= \Studip\Button::create(_("Vorlage verteilen"), 'newvorlage') ?>
-    </div>
-</form>
 
-<form action="<?= URLHelper::getLink("plugins.php/eportfolioplugin/showsupervisor/distributePortfolioContents/". $masterid . '/' . $groupid) ?>">
-    <div data-dialog-button>
-        <?= \Studip\Button::create(_("Fertigstellen"), 'newvorlage', array("data-dialog"=>"close")) ?>
-    </div>
-</form>
+<script>
+
+function exportPortfolio(master){
+
+  urlexport = STUDIP.URLHelper.getURL('plugins.php/courseware/exportportfolio', {cid: master}); //url export
+
+  $.ajax({
+    type: "GET",
+    url: urlexport,
+    success: function(exportData){
+      var path = exportData; //export data
+      console.log("###exportPath:");
+      console.log(path);
+
+      urlimport = STUDIP.URLHelper.getURL('plugins.php/courseware/importportfolio', {cid: 'e10c5a03a8248cbd14abab70f0655475'}); //url import
+
+      targets = <?php echo json_encode($semList);?>;
+      targets.forEach(function(target) {
+
+        urlimport = STUDIP.URLHelper.getURL('plugins.php/courseware/importportfolio', {cid: target}); //url import
+
+        $.ajax({
+          type: "POST",
+          url: urlimport,
+          data: {
+            // xml: xml,
+            master: master,
+            target: target,
+            path: path,
+          },
+          success: function(importData){
+            console.log("###importData:");
+            console.log(importData);
+            closeModal();
+            //location.reload();
+          }
+        });
+      });
+
+    }
+  });
+}
+</script>
