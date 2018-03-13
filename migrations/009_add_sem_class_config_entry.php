c7c36409d6d36a8a7a8ffefb16b6d5a5efc19d06
asudau <sudau@elan-ev.de>
2018-03-13 14:28:21 +0100

    #146

--- migrations/009_add_sem_class_config_entry.php
+++ migrations/009_add_sem_class_config_entry.php
@@ -2,128 +2,38 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-class AddSemClass extends Migration
+class AddSemClassConfigEntry extends Migration
 {
     public function description () {
-        return 'add SemClass and SemTypes whose courses have this plugin in their overview slot';
+        return 'add PORTFOLIO_VORLAGE SemClass configEntry';
     }
 
 
     public function up () {
-        $id = $this->insertSemClass();
+        
+        $sem_type_id;
+        
+        foreach ($GLOBALS['SEM_TYPE'] as $id => $sem_type){ //get the id of ePortfolio Seminarclass
+            if ($sem_type['name'] == 'ePortfolio-Vorlage') {
+                $sem_type_id = $id;
+            }
+        }
+
+        Config::get()->create('SEM_CLASS_PORTFOLIO_VORLAGE', array(
+            'value'       => $sem_type_id,
+            'is_default'  => 0,
+            'type'        => 'integer',
+            'range'       => 'global',
+            'section'     => 'global',
+            'description' => 'ID der Veranstaltungsklasse für Portfolio-Supervisionsgruppen'
+            ));
     }
 
 
     public function down () {
-        // $id = $this->getMoocSemClassID();
-        // $this->removeSemClassAndTypes($id);
-        // $this->removeConfigOption();
-        // SimpleORMap::expireTableScheme();
-
-        $db = DBManager::get();
-
-        //remove entry in sem_classes
-        $name = "ePortfolio";
-        $statement = $db->prepare("DELETE FROM sem_classes WHERE name = ?");
-        $statement->execute(array($name));
-
-        //remove entry in sem_types
-        $nameType = "ePortfolio";
-        $statement = $db->prepare("DELETE FROM sem_types WHERE name = ?");
-        $statement->execute(array($nameType));
+       
     }
 
-    /**********************************************************************/
-    /* PRIVATE METHODS                                                    */
-    /**********************************************************************/
-
-
-    private function insertSemClass()
-    {
-        $db = DBManager::get();
-        $name = "ePortfolio";
-        $nameType = "ePortfolio";
-        $id = -2;
-
-        //Fügt Spalte an true or false ePortfolio
-        // $nameType = "eportfolioStatus";
-        // $statement = $db->prepare("ALTER TABLE seminare ADD ? BOOLEAN");
-        // $statement->execute(array($nameType));
-
-        if($this->validateUniqueness($name)) {
-    			$statement = $db->prepare("INSERT INTO sem_classes SET name = ?, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
-    			$statement->execute(array($name));
-    			$id = $db->lastInsertId();
-
-          //Insert sem_type
-          $statementSemTypes = $db->prepare("INSERT INTO sem_types SET name = ?, class = $id, mkdate = UNIX_TIMESTAMP(), chdate = UNIX_TIMESTAMP()");
-          $statementSemTypes->execute(array($nameType));
-
-    	    } else {
-    			// We already got a type with that name, should be a previous installation ...
-                $statement = $db->prepare('SELECT id FROM sem_classes WHERE name = ?');
-                $statement->execute(array($name));
-                $id = $statement->fetchColumn();
-    		}
-
-    		if($id === -2) {
-    			$message = sprintf('Ungültige id (id=%d)', $id);
-                throw new Exception($message);
-    		}
-
-
-        $sem_class = SemClass::getDefaultSemClass();
-        $sem_class->set('name', $name);
-        $sem_class->set('id', $id);
-        $sem_class->set('studygroup_mode', '1');
-
-        // Setting Mooc-courses default datafields: mooc should not to be disabled, courseware and mooc should be active
-        $current_modules = $sem_class->getModules(); // get modules
-        $current_modules['EportfolioPlugin']['activated'] = '1';
-        $current_modules['EportfolioPlugin']['sticky'] = '1'; 
-        $current_modules['Courseware']['activated'] = '1';   // set values
-        $current_modules['Courseware']['sticky'] = '1'; // sticky = 1 -> can't be chosen in "more"-field of course
-        $current_modules['CoreParticipants']['activated'] = '0';
-        $current_modules['CoreParticipants']['sticky'] = '1'; 
-        $current_modules['CoreDocuments']['activated'] = '0';
-        $current_modules['CoreDocuments']['sticky'] = '1'; 
-        $current_modules['CoreOverview']['activated'] = '0';
-        $current_modules['CoreOverview']['sticky'] = '1'; 
-        $current_modules['CoreAdmin']['activated'] = '0';
-        $current_modules['CoreAdmin']['sticky'] = '1'; 
-        $current_modules['CoreSchedule']['activated'] = '0';
-        $current_modules['CoreSchedule']['sticky'] = '1'; 
-        $current_modules['CoreWiki']['activated'] = '0';
-        $current_modules['CoreWiki']['sticky'] = '1'; 
-        $current_modules['CoreElearningInterface']['activated'] = '0';
-        $current_modules['CoreElearningInterface']['sticky'] = '1'; 
-        
-        
-        $sem_class->set('overview', 'EportfolioPlugin');
-        $sem_class->setModules($current_modules); // set modules
-
-        $sem_class->store();
-
-        return $id;
-    }
-
-    private function validateUniqueness($name)
-    {
-        $statement = DBManager::get()->prepare('SELECT id FROM sem_classes WHERE name = ?');
-        $statement->execute(array($name));
-        if ($old = $statement->fetchColumn()) {
-            // $message = sprintf('Es existiert bereits eine Veranstaltungskategorie mit dem Namen "%s" (id=%d)', htmlspecialchars($name), $old);
-            // throw new Exception($message);
-            return false;
-        }
-        return true;
-    }
-
-    private function removeSemClassAndTypes($id)
-    {
-        $sem_class = new SemClass(intval($id));
-        $sem_class->delete();
-        $GLOBALS['SEM_CLASS'] = SemClass::refreshClasses();
-    }
+    
 
 }
