d42b0298000a67350183cdd7b0921a87c7ba43ff
asudau <sudau@elan-ev.de>
2018-04-18 13:48:22 +0200

    s.o.

--- migrations/012_avatar_migration.php
+++ migrations/012_avatar_migration.php
@@ -2,34 +2,40 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-class LockedBlocksTable extends Migration
+class AvatarMigration extends Migration
 {
     public function description () {
-        return 'add tables for portfolio freigaben';
+        return 'add course avatar and startsemester for portfolio seminare';
     }
 
 
     public function up () {
-        $db = DBManager::get();
-        $db->exec("CREATE TABLE `eportfolio_locked_blocks` (
-          `Seminar_id` varchar(32) NOT NULL,
-          `block_id` int(11) NOT NULL DEFAULT '0',
-          `mkdate` int(11) NOT NULL,
-          `chdate` int(11) NOT NULL,
-          PRIMARY KEY (block_id)
-        ) ");
-       
-        SimpleORMap::expireTableScheme();
+        $seminare = Eportfoliomodel::findBySQL('true');
+        foreach ($seminare as $sem){
+            $course = Course::find($sem->Seminar_id);
+            if($course){
+                $status = $course->status;
+                $avatar = CourseAvatar::getAvatar($sem->Seminar_id);
+                if ($status == Config::get()->getValue('SEM_CLASS_PORTFOLIO_VORLAGE')){
+                    $filename = sprintf('%s/public/plugins_packages/uos/EportfolioPlugin/%s',$GLOBALS['STUDIP_BASE_PATH'],'assets/images/avatare/vorlage.png');
+                    $avatar->createFrom($filename);
+                } else if ($status == Config::get()->getValue('SEM_CLASS_PORTFOLIO')){
+                    $filename = sprintf('%s/public/plugins_packages/uos/EportfolioPlugin/%s',$GLOBALS['STUDIP_BASE_PATH'],'assets/images/avatare/eportfolio.png');
+                    $avatar->createFrom($filename);
+                } else if ($status == Config::get()->getValue('SEM_CLASS_PORTFOLIO_Supervisionsgruppe')){
+                    $filename = sprintf('%s/public/plugins_packages/uos/EportfolioPlugin/%s',$GLOBALS['STUDIP_BASE_PATH'],'assets/images/avatare/supervisorgruppe.png');
+                    $avatar->createFrom($filename);
+                }
+                $current_semester = Semester::findCurrent();
+                $seminar = new Seminar($sem->Seminar_id);
+                $seminar->setEndSemester(-1);
+                $seminar->setStartSemester($current_semester->beginn);
+            } 
+        }
     }
 
 
     public function down () {
-        
-
-        $db = DBManager::get();
-        $db->exec("DROP TABLE locked_blocks");    
-        SimpleORMap::expireTableScheme();
-
     }
 
 
