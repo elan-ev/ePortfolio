e6e529256e9d6182f69e9c16d46560e1dad7600f
asudau <sudau@elan-ev.de>
2018-04-18 13:50:22 +0200

    Grundlage für das Tracken von Aufrufen #150

--- migrations/011_last_visited_table.php
+++ migrations/011_last_visited_table.php
@@ -2,21 +2,20 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-class LockedBlocksTable extends Migration
+class LastVisitedTable extends Migration
 {
     public function description () {
-        return 'add tables for portfolio freigaben';
+        return 'add tables for last visits of portfolio elements';
     }
 
 
     public function up () {
         $db = DBManager::get();
-        $db->exec("CREATE TABLE `eportfolio_locked_blocks` (
-          `Seminar_id` varchar(32) NOT NULL,
-          `block_id` int(11) NOT NULL DEFAULT '0',
-          `mkdate` int(11) NOT NULL,
-          `chdate` int(11) NOT NULL,
-          PRIMARY KEY (block_id)
+        $db->exec("CREATE TABLE `eportfolio_last_visited` (
+          `object_id` varchar(32) NOT NULL,
+          `user_id` varchar(32) NOT NULL DEFAULT '0',
+          `time` int(11) NOT NULL,
+          PRIMARY KEY (object_id, user_id)
         ) ");
        
         SimpleORMap::expireTableScheme();
@@ -27,7 +26,7 @@ class LockedBlocksTable extends Migration
         
 
         $db = DBManager::get();
-        $db->exec("DROP TABLE locked_blocks");    
+        $db->exec("DROP TABLE eportfolio_last_visited");    
         SimpleORMap::expireTableScheme();
 
     }
--- models/LastVisited.class.php
+++ models/LastVisited.class.php
@@ -9,12 +9,11 @@ include_once __DIR__.'/SupervisorGroupUser.class.php';
 /**
  * @author  <asudau@uos.de>
  *
- * @property string     $Seminar_id
- * @property string     $block_id
- * @property int        $mkdate
- * @property int        $chdate
+ * @property string     $object_id
+ * @property string     $user_id
+ * @property int        $time
  */
-class LockedBlock extends SimpleORMap
+class LastVisited extends SimpleORMap
 {
 
     public $errors = array();
@@ -28,28 +27,112 @@ class LockedBlock extends SimpleORMap
      */
     public function __construct($id = null) {
 
-        $this->db_table = 'eportfolio_locked_blocks';
+        $this->db_table = 'eportfolio_last_visited';
 
         parent::__construct($id);
     }
     
-    public static function isLocked($block_id){
-        $entry = self::findById($block_id);
+    //TODO belongs to user ondelete -> delete
+    
+    public static function getLastVisited($object_id, $user_id){
+        $entry = self::findBySQL('object_id = :object_id AND user_id = :user_id');
         if($entry){
-            return true;
+            return $entry->time;
         } else return false;
     }
     
-    public static function lockBlock($Seminar_id, $block_id, $lock){
-        if (($lock == 'true') && !self::findById($block_id)){
-            $lockedBlock = new self($block_id);
-            $lockedBlock->Seminar_id = $Seminar_id;
-            $lockedBlock->mkdate = time();
-            $lockedBlock->chdate = time();
-            $lockedBlock->store();
-        } else if (($lock == 'false') && self::findById($block_id)){
-            self::deleteBySQL('block_id = :block_id',
-                array(':block_id' => $block_id));
+    public static function setVisited($object_id, $user_id){
+        $entry = self::getLastVisited($object_id, $user_id);
+        if ($entry){
+            $entry->time = time();
+            $entry->store();
+        } else {
+            $entry = new LastVisited();
+            $entry->object_id = $object_id;
+            $entry->user_id = $user_id;
+            $entry->time = time();
+            $entry->store();
         }
+    }
+    
+    public static function chapter_last_visited($block_id, $user_id){
+        $last_visited = self::cwblock_last_visited($block_id, $user_id);
+        
+        //block get children
+        $blocks = self::getAllChildBlocks($block_id);
+        //foreach block id get userprogress 
+        foreach($blocks as $block){
+            //if any userprogress > last changed -> content changed = false
+            if (self::cwblock_last_visited($block, $user_id) > $last_visited){
+                $last_visited = self::cwblock_last_visited($block, $user_id);
+            }
+        }
+        return $last_visited;
+     }
+     
+     public static function cwblock_last_visited($block_id, $user_id){
+        $db = DBManager::get();
+        $stmt = $db->prepare('SELECT * FROM mooc_userprogress WHERE block_id = :block_id AND user_id = :user_id');
+        $stmt->execute(array(':block_id' => $block_id, ':user_id' => $user_id));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if (!$row){
+            return 0;
+        } else {
+           $time = new DateTime($row['chdate']);
+           return $time->getTimestamp();
+        }
+     }
+
+     public static function chapter_changed_since_last_visit($block_id, $user_id){
+        $content_changed = true;
+        
+        //block get children
+        $blocks = self::getAllChildBlocks($block_id);
+        //foreach block id get userprogress 
+        foreach($blocks as $block){
+            //if any userprogress > last changed -> content changed = false
+            if (self::cwblock_last_changed($block) < self::cwblock_last_visited($block, $user_id)){
+                $content_changed = false;
+            }
+        }
+        return $content_changed;
+     }
+     
+     public static function cwblock_last_changed($block_id){
+        $db = DBManager::get();
+        $stmt = $db->prepare('SELECT * FROM mooc_blocks WHERE id = :block_id');
+        $stmt->execute(array(':block_id' => $block_id));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if (!$row){
+            return false;
+        } else {
+           return $row['chdate'];
+        }
+     }
+     
+    private static function getAllChildBlocks($chapter_id){
+        $db = DBManager::get();
+        $blocks = array();
+        $query = "SELECT title, id FROM mooc_blocks WHERE parent_id = :id ORDER BY position ASC";
+            $statement = $db->prepare($query);
+            $statement->execute(array(':id'=> $chapter_id));
+            foreach($statement->fetchAll() as $subchapter){
+                array_push($blocks, $subchapter[id]);
+                $query = "SELECT title, id FROM mooc_blocks WHERE parent_id = :id ORDER BY position ASC";
+                $statement = $db->prepare($query);
+                $statement->execute(array(':id'=> $subchapter[id]));
+                foreach($statement->fetchAll() as $section){
+                    array_push($blocks, $section[id]);
+                    $query = "SELECT title, id FROM mooc_blocks WHERE parent_id = :id ORDER BY position ASC";
+                    $statement = $db->prepare($query);
+                    $statement->execute(array(':id'=> $section[id]));
+                    foreach($statement->fetchAll() as $block){
+                        array_push($blocks, $block[id]);
+                    }
+                }
+            }
+        return $blocks;
     }
 }
