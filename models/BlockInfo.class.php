88c2b03f77c8a569d3d491dc7d05d69d26975336
asudau <sudau@elan-ev.de>
2018-07-25 10:58:48 +0200

    fix (es ist zu heiß)

--- models/BlockInfo.class.php
+++ models/BlockInfo.class.php
@@ -36,7 +36,7 @@ class BlockInfo extends SimpleORMap
     }
     
     public static function createEntry($portfolio_id, $block_id, $vorlagen_block_id){
-        $entry = new self(block_id);
+        $entry = new self($block_id);
         $entry->vorlagen_block_id = $vorlagen_block_id;
         $entry->Seminar_id = $portfolio_id;
         $entry->mkdate = time();
