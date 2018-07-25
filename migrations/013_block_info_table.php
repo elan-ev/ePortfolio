bf782f087401a99178b7427d2aba4bfa52403f36
asudau <sudau@elan-ev.de>
2018-07-25 09:07:19 +0200

    first part of #180

---
+++ migrations/013_block_info_table.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+class LockedBlocksTable extends Migration
+{
+    public function description () {
+        return 'add tables for blocks that cant be edited after distribution';
+    }
+
+
+    public function up () {
+        $db = DBManager::get();
+        $db->exec("CREATE TABLE `eportfolio_locked_blocks` (
+          `Seminar_id` varchar(32) NOT NULL,
+          `block_id` int(11) NOT NULL DEFAULT '0',
+          `mkdate` int(11) NOT NULL,
+          `chdate` int(11) NOT NULL,
+          PRIMARY KEY (block_id)
+        ) ");
+       
+        SimpleORMap::expireTableScheme();
+    }
+
+
+    public function down () {
+        
+
+        $db = DBManager::get();
+        $db->exec("DROP TABLE eportfolio_locked_blocks");    
+        SimpleORMap::expireTableScheme();
+
+    }
+
+
+}
