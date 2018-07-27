9e484eac5a7387bbba376230f34bd43dcc2349d7
asudau <sudau@elan-ev.de>
2018-07-27 12:17:21 +0200

    merge

--- controllers/showsupervisor.php
+++ controllers/showsupervisor.php
@@ -55,20 +55,6 @@ class ShowsupervisorController extends StudipController {
         //sidebar
         $sidebar = Sidebar::Get();
 
-<<<<<<< HEAD
-        $group = EportfolioGroup::findBySQL('Seminar_id = :cid', array(':cid' => $id));
-        $this->countActivities = $group[0]->getNumberOfNewActivities(User::findCurrent());
-
-        $navcreate = new LinksWidget();
-        $navcreate->setTitle('Navigation');
-        $navcreate->addLink("�bersicht", PluginEngine::getLink($this->plugin, array(), 'show'));
-        $navcreate->addLink("Supervisionsansicht", 'showsupervisor', null, array('class' => 'active'));
-        $navcreate->addLink("Activity Feed", PluginEngine::getLink($this->plugin, array(), 'showsupervisor/activityfeed'), Icon::create('exclaim-circle', 'new' ), array('class'=>'feed-news', 'data-feed' => $this->countActivities));
-
-        $sidebar->addWidget($navcreate);
-=======
->>>>>>> 5a90da7568723766a4fe1cf34bafeb0c3af4a324
-
         /**
         $nav = new LinksWidget();
         $nav->setTitle(_('Supervisionsgrupppen'));
