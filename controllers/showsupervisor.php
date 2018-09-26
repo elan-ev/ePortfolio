da314ce26b6cbb4b454a1ac688c4a6c3c8f657f9
Marcel Kipp <mkipp@uos.de>
2018-09-26 14:28:53 +0200

    #197

--- controllers/showsupervisor.php
+++ controllers/showsupervisor.php
@@ -289,6 +289,7 @@ class ShowsupervisorController extends StudipController {
       $this->group_id = $group_id;
 
       $user = new User($user_id);
+      $this->user = $user;
       $this->user_id = $user_id;
       $this->vorname = $user['Vorname'];
       $this->nachname = $user['Nachname'];
