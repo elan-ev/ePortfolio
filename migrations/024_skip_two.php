92f9d01fcc7af023ead37ab327f2f93b3ff7412d
tgloeggl <tgloeggl@uos.de>
2020-10-08 13:14:48 +0200

    renumber migrations once again to allow rerunning it

---
+++ migrations/024_skip_two.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__.'/../bootstrap.php';
+require __DIR__.'/../vendor/autoload.php';
+
+class SkipTwo extends Migration
+{
+    public function description () {
+        return 'Stub, does nothing';
+    }
+
+    public function up ()
+    {
+    }
+
+
+    public function down ()
+    {
+    }
+}
